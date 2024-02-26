24d26739b4820daadd750725b28d1e735a379eac
uther-nax <woodichin@outlook.com>
2024-02-26 23:13:23 +0800

    wip: wrap content with keyboard avoiding view

--- src/pages/CreateGroup.tsx
+++ src/pages/CreateGroup.tsx
@@ -9,6 +9,7 @@ import {
   TextInputChangeEventData,
   View,
   SafeAreaView,
+  KeyboardAvoidingView,
 } from 'react-native';
 import { globalContext } from '../context/globalContext';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
@@ -19,13 +20,24 @@ import { EGroup } from '../entities/EGroup';
 import { PredefinedColors } from '../lib/Constants';
 import { LIGHT_DEFAULT_COLOR } from '../theme/color';
 import { PrimaryButton } from '../components/Button';
+import { HeaderHeightContext } from '@react-navigation/elements';
 
 const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+  },
   container: {
+    flex: 1,
+  },
+  content: {
+    flex: 1,
     display: 'flex',
+    position: 'relative',
     justifyContent: 'space-between',
     paddingHorizontal: 16,
-    height: '100%',
+    borderStyle: 'solid',
+    borderColor: 'blue',
+    borderWidth: 1,
   },
   inputs: {
     display: 'flex',
@@ -44,7 +56,6 @@ const styles = StyleSheet.create({
     borderStyle: 'solid',
     borderColor: '#ccc',
     borderWidth: 1,
-    minHeight: 64,
     borderRadius: 4,
   },
   confirmButton: {
@@ -69,6 +80,18 @@ export const CreateGroup: FC<NativeStackScreenProps<RootStackParamList, 'AddGrou
   const [newName, setNewName] = useState<string>('');
   const [newDesc, setDesc] = useState<string>('');
   const { dbConn } = useContext(globalContext);
+
+  const useHeaderHeight = () => {
+    const height = useContext(HeaderHeightContext);
+
+    if (height === undefined) {
+      return 0;
+    }
+
+    return height;
+  };
+
+  console.log(useHeaderHeight(), '-----------');
 
   const updateName = useCallback((_e: NativeSyntheticEvent<TextInputChangeEventData>) => {
     setNewName(_e.nativeEvent.text);
@@ -104,32 +127,35 @@ export const CreateGroup: FC<NativeStackScreenProps<RootStackParamList, 'AddGrou
       });
   }, [dbConn, inputRef, navigation, newDesc, newName]);
   return (
-    <SafeAreaView>
-      <View style={styles.container}>
-        {/* top, inputs */}
-        <View style={styles.inputs}>
-          <View>
-            <Text style={styles.title}>name</Text>
-            <TextInput style={styles.nameInput} onChange={updateName} ref={inputRef} />
-          </View>
-          <View>
-            <Text style={styles.title}>description</Text>
-            <TextInput style={styles.descInput} multiline={true} onChange={updateDesc} />
-          </View>
-          {/* color picker */}
-          <View>
-            <Text style={styles.title}>Color</Text>
+    <SafeAreaView style={styles.root}>
+      <KeyboardAvoidingView style={styles.container} behavior="padding" keyboardVerticalOffset={98}>
+        <View style={styles.content}>
+          {/* top, inputs */}
+          <View style={styles.inputs}>
             <View>
-              {PredefinedColors.dark.map((color) => (
-                <ColorButton key={color} isDark={true} color={color} onClick={onSelectColor} />
-              ))}
-              <Text>color</Text>
+              <Text style={styles.title}>name</Text>
+              <TextInput style={styles.nameInput} onChange={updateName} ref={inputRef} />
+            </View>
+            <View>
+              <Text style={styles.title}>description</Text>
+              <TextInput style={styles.descInput} multiline={true} onChange={updateDesc} />
+            </View>
+            {/* color picker */}
+            <View style={{ flex: 1 }}>
+              <Text style={styles.title}>Color</Text>
+              <View>
+                {PredefinedColors.dark.map((color) => (
+                  <ColorButton key={color} isDark={true} color={color} onClick={onSelectColor} />
+                ))}
+                <Text>color</Text>
+              </View>
             </View>
           </View>
+          {/* bottom buttons */}
+          <TextInput style={styles.descInput} onChange={updateDesc} />
+          {/* <PrimaryButton pressHandler={addTag} name="confirm" /> */}
         </View>
-        {/* bottom buttons */}
-        <PrimaryButton pressHandler={addTag} name="confirm" />
-      </View>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 };
