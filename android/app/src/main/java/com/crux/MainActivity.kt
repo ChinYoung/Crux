ceb19658f34244e2e27ac4d291e0fb89c9384986
uther-nax <woodichin@outlook.com>
2024-05-09 22:56:29 +0800

    refactor: 💡 update rn to 0.74

--- android/app/src/main/java/com/crux/MainActivity.kt
+++ android/app/src/main/java/com/crux/MainActivity.kt
@@ -1,14 +1,18 @@
-package com.checklisty072
+package com.crux
+
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+
 class MainActivity : ReactActivity() {
+
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "checklisty072"
+  override fun getMainComponentName(): String = "crux"
+
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
    * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
--- ios/cruxTests/cruxTests.m
+++ ios/cruxTests/cruxTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface checklisty072Tests : XCTestCase
+@interface cruxTests : XCTestCase
 
 @end
 
-@implementation checklisty072Tests
+@implementation cruxTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
